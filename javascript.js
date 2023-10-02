71f3e5ccdcf60c085331f699ee12939947021fc3
FriFeb <51440204+FriFeb@users.noreply.github.com>
2023-10-02 20:00:13 +0300

    Create operate function

--- javascript.js
+++ javascript.js
@@ -10,3 +10,22 @@ function multiply(a, b) {
 function divide(a, b) {
   return a / b;
 }
+
+function operate(sign, a, b) {
+  switch (sign) {
+    case "+":
+      add(a, b);
+      break;
+    case "-":
+      subtract(a, b);
+      break;
+    case "*":
+      multiply(a, b);
+      break;
+    case "/":
+      divide(a, b);
+      break;
+    default:
+      return "error";
+  }
+}
