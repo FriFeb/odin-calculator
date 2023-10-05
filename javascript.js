bad90747a9b8f2cc150b77f4b1ee159294461611
FriFeb <51440204+FriFeb@users.noreply.github.com>
2023-10-05 21:59:39 +0300

    add comments of the new approach

--- javascript.js
+++ javascript.js
@@ -20,9 +20,9 @@ function divide(a, b) {
   return a / b;
 }
 
-function operate(sign, a, b) {
-  console.log({ sign, a, b });
-  switch (sign) {
+function operate(operator, a, b) {
+  console.log({ operator, a, b });
+  switch (operator) {
     case "+":
       return add(a, b);
     case "-":
@@ -30,84 +30,189 @@ function operate(sign, a, b) {
     case "*":
       return multiply(a, b);
     case "/":
+      if (b === 0) return "error"
       return divide(a, b);
     default:
       return "error";
   }
 }
 
-function showDigit(e) {
-  if (e.target.tagName !== "BUTTON") return;
-
-  if (isPrintedResult && !isUsedOperator) {
-    displayText.innerHTML = "";
-  }
-
-  displayText.innerHTML += e.target.innerHTML;
-
-  isPrintedResult = false;
-}
-
 /*
-  I can check if display shows 
-    one number
-      then I can add sign operator
-      if sign added I can change sign without rewriting number
-    two numbers 
-      then first two numbers operates with operate() func
-      the result is shown and chosen operator after it
+  Types of buttons: 
+
+    - Operators
+    - Digits
+    - Equals 
+    - Clear
+
+
+  OK, so we can have some states of display in our calculator
+  and we should think how will 
+  different types of buttons act on each state
+
+    - nothing is shown (actually empty display string)
+      + operators cannot be triggered
+      + equals cannot be triggered
+      + digits add one digit
+
+    - one number is shown 
+      + operators grab the number and append used operator
+      + equals shows error
+      + digits add one digit
+
+    - one result number is shown 
+      + operators grab the number and append used operator
+      + equals shows error
+      + digits rewrite the display with new digit
+
+    - one number and sign are shown
+      + operators grab the number and append used operator
+      + equals shows error
+      + digits add one digit
+
+    - two numbers and a sign between them are shown
+      + operators call showResult() then 
+        grab the result number and append used operator
+      + equals shows the result
+      + digits add one digit
+
+    - error is shown
+      + operators cannot be triggered
+      + equals cannot be triggered
+      + digits rewrite the display with new digit
+
+    CLEAR button should always work no matter what display shows
+
+
+  Seems like we need some indicators for our initial states:
+
+    - isEmptyDisplay = true
+    - isResultDisplay = false
+    - isErrorDisplay = false
+    - isTwoOperandsDisplay = false
+
+    
+  And finally functions:
+
+    * showDigit ()
+        IF isResultDisplay is true || isErrorDisplay is true
+          TRUE
+            call clearDisplay()
+
+        add one digit to the display string
+        isEmptyDisplay = false
+
+    * showOperator () 
+        IF isEmptyDisplay is true || isErrorDisplay is true
+          TRUE
+            return
+
+        IF isTwoOperandsDisplay is true
+          TRUE
+            call showResult()
+        
+        grab the first number and add an operator
+        isResultDisplay = false
+
+    * showResult()
+        IF isEmptyDisplay is true || isErrorDisplay is true
+          TRUE
+            return
+
+        IF isTwoOperandsDisplay is false
+          TRUE
+            call showError()
+            return
+
+        declare variable result and call operate() in it
+        IF result is "error"
+          TRUE
+            showError()
+            return
+        
+        show result in the display string
+        isResultDisplay = true
+        isTwoOperandsDisplay = false
+
+    * clearDisplay()
+        reset display string 
+        isEmptyDisplay = true
+        isResultDisplay = false
+        isErrorDisplay = false
+        isTwoOperandsDisplay = false
+
+    * showError()
+        call clearDisplay()
+        show error message in the display string
+        isEmptyDisplay = false
+        isErrorDisplay = true
+        
+    Yeah, that's pretty much it, 
+    hope it will cover all needed situations
 */
 
-function calculateDisplayNumbers() {
-  const destructuredDisplayString = displayText.innerText.split(" ");
-  if (!destructuredDisplayString[0].length) return 0;
-  else if (destructuredDisplayString.length === 3) return 2;
-  else return 1;
-}
+// function showDigit(e) {
+//   if (e.target.tagName !== "BUTTON") return;
 
-function showOperator(e) {
-  if (e.target.tagName !== "BUTTON") return;
+//   if (isPrintedResult && !isUsedOperator) {
+//     displayText.innerHTML = "";
+//   }
 
-  const numberCount = calculateDisplayNumbers();
+//   displayText.innerHTML += e.target.innerHTML;
 
-  if (!numberCount) return;
-  else if (numberCount === 2) {
-    showResult(e);
-  }
+//   isPrintedResult = false;
+// }
 
-  const destructuredDisplayString = displayText.innerText.split(" ");
-  const [firstNumber] = destructuredDisplayString;
+// function calculateDisplayNumbers() {
+//   const destructuredDisplayString = displayText.innerText.split(" ");
+//   if (!destructuredDisplayString[0].length) return 0;
+//   else if (destructuredDisplayString.length === 3) return 2;
+//   else return 1;
+// }
 
-  if (isNaN(firstNumber)) return;
-  
-  displayText.innerHTML = `${firstNumber} ${e.target.innerHTML} `;
-  isUsedOperator = true;
-}
+// function showOperator(e) {
+//   if (e.target.tagName !== "BUTTON") return;
 
-function showResult(e) {
-  if (e.target.tagName !== "BUTTON") return;
+//   const numberCount = calculateDisplayNumbers();
 
-  isPrintedResult = true;
-  isUsedOperator = false;
+//   if (!numberCount) return;
+//   else if (numberCount === 2) {
+//     showResult(e);
+//   }
 
-  const destructuredDisplayString = displayText.innerText.split(" ");
+//   const destructuredDisplayString = displayText.innerText.split(" ");
+//   const [firstNumber] = destructuredDisplayString;
 
-  const [firstNumber, , secondNumber] = destructuredDisplayString;
-  const operator = destructuredDisplayString[1];
+//   if (isNaN(firstNumber)) return;
 
-  if (
-    !firstNumber ||
-    !secondNumber ||
-    (operator === "/" && +secondNumber === 0)
-  ) {
-    displayText.innerHTML = "Error!";
-    return;
-  }
+//   displayText.innerHTML = `${firstNumber} ${e.target.innerHTML} `;
+//   isUsedOperator = true;
+// }
 
-  const result = operate(operator, +firstNumber, +secondNumber);
+// function showResult(e) {
+//   if (e.target.tagName !== "BUTTON") return;
 
-  displayText.innerText = Math.round(result * 1000) / 1000;
-}
+//   isPrintedResult = true;
+//   isUsedOperator = false;
+
+//   const destructuredDisplayString = displayText.innerText.split(" ");
+
+//   const [firstNumber, , secondNumber] = destructuredDisplayString;
+//   const operator = destructuredDisplayString[1];
+
+//   if (
+//     !firstNumber ||
+//     !secondNumber ||
+//     (operator === "/" && +secondNumber === 0)
+//   ) {
+//     displayText.innerHTML = "Error!";
+//     return;
+//   }
+
+//   const result = operate(operator, +firstNumber, +secondNumber);
+
+//   displayText.innerText = Math.round(result * 1000) / 1000;
+// }
 
 function clearDisplay() {
   displayText.innerHTML = "";
