1a88c47cb019ae0729b1e705fd9de59d6d11f9f3
FriFeb <51440204+FriFeb@users.noreply.github.com>
2023-10-09 14:48:50 +0300

    Rewtite solution with new approach

--- javascript.js
+++ javascript.js
@@ -1,12 +1,13 @@
+const displayText = document.querySelector(".display-text");
 const operatorBtns = document.querySelector(".operators");
 const digitBtns = document.querySelector(".digits");
-const displayText = document.querySelector(".display-text");
 const equalBtn = document.querySelector(".equal");
 const clearBtn = document.querySelector(".clear");
 
 let isEmptyDisplay = true;
 let isResultDisplay = false;
 let isErrorDisplay = false;
+let isOperatorDisplay = false;
 
 function add(a, b) {
   return a + b;
@@ -21,8 +22,7 @@ function divide(a, b) {
   return a / b;
 }
 
-function operate(operator, a, b) {
-  console.log({ operator, a, b });
+function operate(a, b, operator) {
   switch (operator) {
     case "+":
       return add(a, b);
@@ -38,230 +38,161 @@ function operate(operator, a, b) {
   }
 }
 
-/*
-  Types of buttons: 
+function destructureDisplayString() {
+  const displayStr = displayText.innerText;
+  const expression = displayStr.split(" ");
+  return [+expression[0], +expression[2], expression[1]];
+}
 
-    - Operators
-    - Digits
-    - Equals 
-    - Clear
+// function isTooBigOperand(operand) {
+//   if (operand / 1e5 > 1) return true;
+// }
 
+// function isTooBigCurrentOperand() {
+//   const [operand1, operand2] = destructureDisplayString();
 
-  OK, so we can have some states of display in our calculator
-  and we should think how will 
-  different types of buttons act on each state
-
-    - nothing is shown (actually empty display string)
-      + operators cannot be triggered
-      + equals cannot be triggered
-      + digits add one digit
-
-    - one number is shown 
-      + operators grab the number and append used operator
-      + equals shows error
-      + digits add one digit
-
-    - one result number is shown 
-      + operators grab the number and append used operator
-      + equals shows error
-      + digits rewrite the display with new digit
-
-    - one number and sign are shown
-      + operators grab the number and append used operator
-      + equals shows error
-      + digits add one digit
-
-    - two numbers and a sign between them are shown
-      + operators call showResult() then 
-        grab the result number and append used operator
-      + equals shows the result
-      + digits add one digit
-
-    - error is shown
-      + operators cannot be triggered
-      + equals cannot be triggered
-      + digits rewrite the display with new digit
-
-    CLEAR button should always work no matter what display shows
-
-
-  Seems like we need some indicators for our initial states:
-
-    - isEmptyDisplay = true
-    - isResultDisplay = false
-    - isErrorDisplay = false
-    
-  And finally functions:
-
-    * showDigit ()
-        IF isResultDisplay is true || isErrorDisplay is true
-          TRUE
-            call clearDisplay()
-
-        IF isTooBigCurrentOperand() is true
-          TRUE 
-            alert("Too big number")
-
-        add one digit to the display string
-        isEmptyDisplay = false
-
-    * showOperator () 
-        IF isEmptyDisplay is true || isErrorDisplay is true
-          TRUE
-            return
-
-        IF isTwoOperandsDisplay() is true
-          TRUE
-            call showResult()
-        
-        grab the first number and add an operator
-        isResultDisplay = false
-
-    * showResult()
-        IF isEmptyDisplay is true || isErrorDisplay is true
-          TRUE
-            return
-
-        IF isTwoOperandsDisplay() is false
-          TRUE
-            call showError()
-            return
-
-        declare variable result and call operate() in it
-        IF result is "error"
-          TRUE
-            showError()
-            return
-        
-        show result in the display string
-        isResultDisplay = true
-
-    * clearDisplay()
-        reset display string 
-        isEmptyDisplay = true
-        isResultDisplay = false
-        isErrorDisplay = false
-
-    * showError()
-        call clearDisplay()
-        show error message in the display string
-        isEmptyDisplay = false
-        isErrorDisplay = true
-
-    * destructureDisplayString() 
-        grab the display string
-        split it with spaces into array
-        return operand1 and operand2
-
-    * isTwoOperandsDisplay()
-        operand1 and operand2 = destructureDisplayString() 
-
-        IF operand1.length is true && operand2.length is true
-          TRUE
-            return true
-        return false
-
-    * isTooBigCurrentOperand () 
-        operand1 and operand2 = destructureDisplayString() 
-
-        IF operand2 
-          TRUE
-            IF isTooBigOperand (operand2) === true
-              TRUE 
-                return true
-
-        IF isTooBigOperand (operand1) === true
-          TRUE 
-            return true
-
-    * isTooBigOperand (operand)
-        IF operand.length > 10^6
-          return true
-          
-        
-    Yeah, that's pretty much it, 
-    hope it will cover all needed situations
-*/
-
-// function showDigit(e) {
-//   if (e.target.tagName !== "BUTTON") return;
-
-//   if (isPrintedResult && !isUsedOperator) {
-//     displayText.innerHTML = "";
+//   if (isOperatorDisplay) {
+//     return isTooBigOperand(operand2);
+//   } else {
+//     return isTooBigOperand(operand1);
 //   }
-
-//   displayText.innerHTML += e.target.innerHTML;
-
-//   isPrintedResult = false;
 // }
 
-// function calculateDisplayNumbers() {
-//   const destructuredDisplayString = displayText.innerText.split(" ");
-//   if (!destructuredDisplayString[0].length) return 0;
-//   else if (destructuredDisplayString.length === 3) return 2;
-//   else return 1;
-// }
+function isTwoOperandDisplay() {
+  const [operand1, operand2] = destructureDisplayString();
 
-// function showOperator(e) {
-//   if (e.target.tagName !== "BUTTON") return;
-
-//   const numberCount = calculateDisplayNumbers();
-
-//   if (!numberCount) return;
-//   else if (numberCount === 2) {
-//     showResult(e);
-//   }
-
-//   const destructuredDisplayString = displayText.innerText.split(" ");
-//   const [firstNumber] = destructuredDisplayString;
-
-//   if (isNaN(firstNumber)) return;
-
-//   displayText.innerHTML = `${firstNumber} ${e.target.innerHTML} `;
-//   isUsedOperator = true;
-// }
-
-// function showResult(e) {
-//   if (e.target.tagName !== "BUTTON") return;
-
-//   isPrintedResult = true;
-//   isUsedOperator = false;
-
-//   const destructuredDisplayString = displayText.innerText.split(" ");
-
-//   const [firstNumber, , secondNumber] = destructuredDisplayString;
-//   const operator = destructuredDisplayString[1];
-
-//   if (
-//     !firstNumber ||
-//     !secondNumber ||
-//     (operator === "/" && +secondNumber === 0)
-//   ) {
-//     displayText.innerHTML = "Error!";
-//     return;
-//   }
-
-//   const result = operate(operator, +firstNumber, +secondNumber);
-
-//   displayText.innerText = Math.round(result * 1000) / 1000;
-// }
+  if (!isNaN(operand1) && !isNaN(operand2)) return true;
+}
 
 function showDigit(e) {
   if (e.target.tagName !== "BUTTON") return;
 
-  if (isResultDisplay === true || isErrorDisplay === true) {
+  if (isResultDisplay || isErrorDisplay) {
     clearDisplay();
   }
 
+  // if (isTooBigCurrentOperand()) {
+  //   alert("Too big number!");
+  //   return;
+  // }
+
   displayText.innerHTML += e.target.innerHTML;
-  if (isEmptyDisplay) isEmptyDisplay = false;
+  isEmptyDisplay = false;
+}
+
+function showOperator(e) {
+  if (e.target.tagName !== "BUTTON") return;
+
+  if (isEmptyDisplay || isErrorDisplay) return;
+
+  if (isTwoOperandDisplay()) {
+    showResult(e);
+  }
+
+  const [operand1] = destructureDisplayString();
+
+  displayText.innerHTML = `${operand1} ${e.target.innerText} `;
+  isResultDisplay = false;
+  isOperatorDisplay = true;
+}
+
+function showResult(e) {
+  if (e.target.tagName !== "BUTTON") return;
+
+  if (isEmptyDisplay || isErrorDisplay) return;
+
+  if (!isTwoOperandDisplay()) {
+    showError();
+    return;
+  }
+
+  const result = operate(...destructureDisplayString());
+
+  if (result === "error") {
+    showError();
+    return;
+  }
+
+  displayText.innerHTML = Math.trunc(result * 1000) / 1000;
+  isResultDisplay = true;
+  isOperatorDisplay = false;
 }
 
 function clearDisplay() {
   displayText.innerHTML = "";
+  isEmptyDisplay = true;
+  isResultDisplay = false;
+  isErrorDisplay = false;
+  isOperatorDisplay = false;
+}
+
+function showError() {
+  clearDisplay();
+  displayText.innerHTML = "Error!";
+  isEmptyDisplay = false;
+  isErrorDisplay = true;
 }
 
 digitBtns.addEventListener("click", showDigit);
-// operatorBtns.addEventListener("click", showOperator);
-// equalBtn.addEventListener("click", showResult);
+operatorBtns.addEventListener("click", showOperator);
+equalBtn.addEventListener("click", showResult);
 clearBtn.addEventListener("click", clearDisplay);
+
+document.addEventListener("keydown", (e) => {
+  const regex = /[0-9]/;
+  const digit = e.code.match(regex);
+
+  switch (e.code) {
+    case `Digit${digit}`:
+      if (e.shiftKey && +digit[0] === 8) {
+        document.querySelector(".multiply").click();
+        break;
+      }
+    case `Numpad${digit}`:
+      document.querySelector(`.digit-${digit}`).click();
+      break;
+
+    case "NumpadAdd":
+      document.querySelector(".add").click();
+      break;
+    case "Equal":
+      if (e.shiftKey) {
+        document.querySelector(".add").click();
+      } else {
+        document.querySelector(".equal").click();
+      }
+      break;
+
+    case "NumpadSubtract":
+    case "Minus":
+      document.querySelector(".minus").click();
+      break;
+
+    case "NumpadMultiply":
+      document.querySelector(".multiply").click();
+      break;
+    case "Digit8":
+      if (e.shiftKey) document.querySelector(".multiply").click();
+      break;
+
+    case "NumpadDivide":
+    case "Slash":
+      document.querySelector(".divide").click();
+      break;
+
+    case "Enter":
+    case "NumpadEnter":
+      document.querySelector(".equal").click();
+      break;
+
+    case "NumpadDecimal":
+    case "Delete":
+      document.querySelector(".clear").click();
+      break;
+
+    case "Backspace":
+      displayText.innerHTML = displayText.innerHTML.slice(0, -1);
+      isResultDisplay = false;
+      break;
+  }
+});
